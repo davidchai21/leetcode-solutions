39ac945a80541efa79b7c312f0bd12f9fda45a55
davidchai21 <rchai@email.wm.edu>
2017-09-24 16:15:31 -0400

    medium

--- 535 Encode and Decode TinyURL/EncodeAndDecodeTinyURL.cpp
+++ 535 Encode and Decode TinyURL/EncodeAndDecodeTinyURL.cpp
@@ -3,18 +3,30 @@ public:
 
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        l=longUrl;
-        return s;
+        if (ls.find(longUrl)!=ls.end()) return ls[longUrl];
+        else {
+            string t="";
+            long n=id;
+            id++;
+            do {
+                t=dict[n%62]+t;
+                n/=62;
+            } while (n);
+            ls[longUrl]=t;
+            sl[t]=longUrl;
+            return t;
+        }
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        s=shortUrl;
-        return l;
+        return sl[shortUrl];
     }
 private:
-    string s;
-    string l;
+    unordered_map<string,string> ls;
+    unordered_map<string,string> sl;
+    long id=0;
+    string dict="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 };
 
 // Your Solution object will be instantiated and called as such:
