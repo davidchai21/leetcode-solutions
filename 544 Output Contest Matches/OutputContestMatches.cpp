9cc7b83ea2cef5887cffee31bf9bc29a6b06c9a5
davidchai21 <rchai@email.wm.edu>
2017-09-24 15:59:21 -0400

    medium

---
+++ 544 Output Contest Matches/OutputContestMatches.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string findContestMatch(int n) {
+        vector<string> m(n);
+        for (int i=1;i<=n;i++) m[i-1]=to_string(i);
+        while (n>1) {
+            for (int i=0;i<n/2;i++)
+            {
+                m[i]="("+m[i]+","+m[n-1-i]+")";
+            }
+            n/=2;
+        }
+        return m[0];
+    }
+};
---
+++ 544 Output Contest Matches/README.md
@@ -0,0 +1,3 @@
+This question is easy. Just initialize a size n string vector and do the concatenation.
+
+Runtime: 3ms
